47a7fea1f799ef799064999ba6147aa900997d8f
Maxwell Carlson <bart4282819@gmail.com>
2018-06-01 23:53:38 -0700

    75% accuracy on the moves out of the 70 game set, and 96% accuracy predicting moves from a sepperate set of 15 games. All training and test games are from 9D or higher pro go players

--- GoClassifier/GoClassifier.py
+++ GoClassifier/GoClassifier.py
@@ -17,40 +17,44 @@ sess = tf.Session(config=tf.ConfigProto(log_device_placement=True))
 
 BoardSize = 361
 
-gdataSize = str(15)
-gtestSize = str(15)
+gdataSize = str(70)
+testDataSz = str(15)
 fileNameX = 'goData' + gdataSize + '.csv'
 fileNameY = 'goDataRes' + gdataSize + '.csv'
-fnameTestX = gtestSize + 'Test.csv'
-fnameTestY = gtestSize + 'TestRes.csv'
+fileNameXt = 'test' + testDataSz + '.csv'
+fileNameYt = 'test' + testDataSz + 'Res.csv'
 
-#import numpy
-#X  = numpy.random.random_integers(0, 1, (1500, BoardSize))
-#Y  = numpy.random.random_integers(0, 1, (1500, BoardSize))
+WeightPath = "Weights.h5"
 
-import numpy as np
+#import numpy as np
+#X  = np.random.random_integers(0, 1, (1500, BoardSize))
+#Y  = np.random.random_integers(0, 1, (1500, BoardSize))
+
+
 from numpy import genfromtxt
 
-
 X = genfromtxt(fileNameX, delimiter=',')
-
-print(X[0])
-
 Y = genfromtxt(fileNameY, delimiter=',')
 Y = tf.keras.utils.to_categorical(Y, BoardSize)
 
-Xt = genfromtxt(fnameTestX, delimiter=',')
-Yt = genfromtxt(fnameTestY, delimiter=',')
+Xt = genfromtxt(fileNameXt, delimiter=',')
+Yt = genfromtxt(fileNameYt, delimiter=',')
+Yt = tf.keras.utils.to_categorical(Yt, BoardSize)
 
 hiddenSize = 1024
 
 
 model = tf.keras.Sequential([
     tf.keras.layers.Dense(hiddenSize, activation='relu', input_shape=(BoardSize,)),
+    tf.keras.layers.Dropout(0.4),
     tf.keras.layers.Dense(hiddenSize, activation='relu'),
+    tf.keras.layers.Dropout(0.4),
     tf.keras.layers.Dense(hiddenSize, activation='relu'),
+    tf.keras.layers.Dropout(0.4),
     tf.keras.layers.Dense(hiddenSize, activation='relu'),
+    tf.keras.layers.Dropout(0.4),
     tf.keras.layers.Dense(hiddenSize, activation='relu'),
+    tf.keras.layers.Dropout(0.4),
     tf.keras.layers.Dense(BoardSize, activation='softmax')
 ])
 
@@ -67,8 +71,10 @@ optimizer = tf.train.AdamOptimizer(learning_rate=0.001)
 model.compile(optimizer=optimizer, loss='categorical_crossentropy', metrics=['accuracy']) # Look into using to_catagorical on outputs instead of generating huge arrays of data
 
 #with tf.device('/cpu:0'):
-model.fit(X, Y, validation_data=(Xt, Yt), epochs=2000, batch_size=1000, verbose=2)
+model.fit(X, Y, validation_data=(Xt, Yt), epochs=2000, batch_size=2000, verbose=2)
 #
+
+model.save_weights(WeightPath)
 
 #tf.keras.layers.Convolution1D(32, 3, 3, activation='relu', input_shape=(None,BoardSize)),
 #tf.keras.layers.Convolution1D(32, 3, 3, activation='relu'),
