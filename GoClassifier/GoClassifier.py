9c843f4cd16ad4a7b77f0152b61cfc3e2968f77d
Maxwell Carlson <bart4282819@gmail.com>
2018-06-01 22:38:47 -0700

    Finally might have figured out what was going wrong with training. It appears I've been curating the data poorly and there are NAN's in the board position arrays

--- GoClassifier/GoClassifier.py
+++ GoClassifier/GoClassifier.py
@@ -8,7 +8,7 @@ import tensorflow.contrib.eager as tfe
 
 from keras import backend as K
 
-tf.enable_eager_execution()
+#tf.enable_eager_execution()
 
 sess = tf.Session()
 K.set_session(sess)
@@ -17,124 +17,64 @@ sess = tf.Session(config=tf.ConfigProto(log_device_placement=True))
 
 BoardSize = 361
 
-gdataSize = str(10)
+gdataSize = str(15)
 gtestSize = str(15)
 fileNameX = 'goData' + gdataSize + '.csv'
 fileNameY = 'goDataRes' + gdataSize + '.csv'
 fnameTestX = gtestSize + 'Test.csv'
 fnameTestY = gtestSize + 'TestRes.csv'
 
+#import numpy
+#X  = numpy.random.random_integers(0, 1, (1500, BoardSize))
+#Y  = numpy.random.random_integers(0, 1, (1500, BoardSize))
+
+import numpy as np
 from numpy import genfromtxt
 
 
-Xt = genfromtxt(fileNameX, delimiter=',')
-Yt = genfromtxt(fileNameY, delimiter=',')
-X = genfromtxt(fnameTestX, delimiter=',')
-Y = genfromtxt(fnameTestY, delimiter=',')
+X = genfromtxt(fileNameX, delimiter=',')
+
+print(X[0])
+
+Y = genfromtxt(fileNameY, delimiter=',')
+Y = tf.keras.utils.to_categorical(Y, BoardSize)
+
+Xt = genfromtxt(fnameTestX, delimiter=',')
+Yt = genfromtxt(fnameTestY, delimiter=',')
 
 hiddenSize = 1024
 
 
 model = tf.keras.Sequential([
-    tf.keras.layers.Dense(hiddenSize,  activation="relu", input_shape=(BoardSize,)), #BoardSize
-    tf.keras.layers.Dense(hiddenSize, activation="relu"),
-    tf.keras.layers.Dense(hiddenSize, activation="relu"),
-    tf.keras.layers.Dense(hiddenSize, activation="relu"),
-    tf.keras.layers.Dense(hiddenSize, activation="relu"),
-    tf.keras.layers.Dense(hiddenSize, activation="relu"),
-    tf.keras.layers.Dense(hiddenSize, activation="relu"),
-    tf.keras.layers.Dense(hiddenSize, activation="relu"),
-    #tf.keras.layers.Dropout(0.01),
-    tf.keras.layers.Dense(BoardSize) # One output probability for each board location ( Should probably include pass in future)
+    tf.keras.layers.Dense(hiddenSize, activation='relu', input_shape=(BoardSize,)),
+    tf.keras.layers.Dense(hiddenSize, activation='relu'),
+    tf.keras.layers.Dense(hiddenSize, activation='relu'),
+    tf.keras.layers.Dense(hiddenSize, activation='relu'),
+    tf.keras.layers.Dense(hiddenSize, activation='relu'),
+    tf.keras.layers.Dense(BoardSize, activation='softmax')
 ])
 
-#optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.01)
-optimizer = tf.train.AdamOptimizer(learning_rate=0.1)
-#optimizer = tf.train.AdagradOptimizer(learning_rate=0.0001)
 
-model.compile(optimizer=optimizer, loss='logcosh', metrics=['accuracy'])
+model.summary()
 
-model.fit(X, Y,  epochs=200, batch_size=1000, verbose=2)
-#validation_data=(Xt, Yt),
+#optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.1)
+optimizer = tf.train.AdamOptimizer(learning_rate=0.001)
+#optimizer = tf.train.AdagradOptimizer(learning_rate=0.01)
+#optimizer = tf.train.AdadeltaOptimizer(0.01)
+#optimizer = tf.train.RMSPropOptimizer(0.0001)
+#optimizer = tf.train.MomentumOptimizer(0.01, 1)
 
+model.compile(optimizer=optimizer, loss='categorical_crossentropy', metrics=['accuracy']) # Look into using to_catagorical on outputs instead of generating huge arrays of data
 
+#with tf.device('/cpu:0'):
+model.fit(X, Y, validation_data=(Xt, Yt), epochs=2000, batch_size=1000, verbose=2)
+#
 
+#tf.keras.layers.Convolution1D(32, 3, 3, activation='relu', input_shape=(None,BoardSize)),
+#tf.keras.layers.Convolution1D(32, 3, 3, activation='relu'),
+#tf.keras.layers.MaxPooling1D(pool_size=2),
+#tf.keras.layers.Flatten(),
+#tf.keras.layers.Dense(hiddenSize, activation='relu'),
+#tf.keras.layers.Dense(hiddenSize, activation='relu'),
+#tf.keras.layers.Dense(BoardSize, activation='softmax')
 
-
-
-
-
-
-
-"""
-
-def parseCsv(line):
-    defaults = [[0.]] * (BoardSize * 2)
-    parsedLine = tf.decode_csv(line, defaults)
-    # First BoardSize(^2) numbers are the padded label
-    label = tf.reshape(tf.cast(parsedLine[0:BoardSize], tf.int32), [BoardSize]) 
-    # Next are the pieces on the board (-1 for opponent, 0 empty, 1 us)
-    features = tf.reshape(parsedLine[BoardSize:BoardSize*2], [BoardSize])
-
-    return features, label
-
-trainDataset = tf.data.TextLineDataset(fileName)
-trainDataset = trainDataset.map(parseCsv)
-#trainDataset = trainDataset.shuffle(bufferSize=1000)
-trainDataset = trainDataset.batch(361) # Change back to 32 when done debugging
-
-hiddenSize = 32
-
-model = tf.keras.Sequential([
-    tf.keras.layers.Dense(BoardSize, activation="relu", input_shape=(BoardSize,)), #BoardSize
-    tf.keras.layers.Dense(hiddenSize, activation="relu"),
-    tf.keras.layers.Dense(BoardSize) # One output probability for each board location ( Should probably include pass in future)
-])
-
-def loss(model, X, y):
-    y_ = model(X)
-    #return tf.losses.sparse_softmax_cross_entropy(labels=y, logits=y_)
-    #return tf.losses.softmax_cross_entropy(onehot_labels=y, logits=y_)
-    return tf.losses.sigmoid_cross_entropy(multi_class_labels=y, logits=y_)
-
-def grad(model, inputs, targets):
-    with tf.GradientTape() as tape:
-        lossValue = loss(model, inputs, targets)
-    return tape.gradient(lossValue, model.variables)
-
-# Revisit optimizer once working, this will likely be bad for this model
-optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.01)
-
-model.compile(optimizer=optimizer, loss='categorical_crossentropy', metrics=['accuracy'])
-model.fit(x=train_X, y=train_y, epochs=200, batch_size=64, verbose=2)
-
-epochCount = 200
-trainingLoss = []
-traingingAcc = []
-
-
-for epoch in range(epochCount):
-    epochLossAvg = tfe.metrics.Mean()
-    epochAcc = tfe.metrics.Accuracy()
-
-    # Training loop - using batchSize batches
-    with tf.device('/gpu:0'):
-        for X, y in trainDataset:
-            grads = grad(model, X, y)
-            optimizer.apply_gradients(zip(grads, model.variables), 
-                                        global_step=tf.train.get_or_create_global_step())
-     
-            # Record current loss
-            epochLossAvg(loss(model, X, y))
-
-            # Compare prediction to actual
-            epochAcc(tf.argmax(model(X), axis=1, output_type=tf.int32), y)
-
-    trainingLoss.append(epochLossAvg.result())
-    traingingAcc.append(epochAcc.result())
-
-    if(epoch % 2 == 0):
-        print("Epoch {:03d}: Loss: {:.3f}, Accuracy: {:.3%}".format(epoch,
-                                                                epochLossAvg.result(),
-                                                                epochAcc.result()))
-"""
